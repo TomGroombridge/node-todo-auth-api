9cefb420fe19e9c06614c41cab2219ea6c85c99f
Tom Groombridge <tom.groombridge@zopa.com>
2022-07-20 17:50:10 +0100

    push new code

--- index.js
+++ index.js
@@ -3,10 +3,26 @@ const bodyParser = require('body-parser')
 const app = express()
 const port = 3001
 const dotenv = require('dotenv');
+const { expressjwt: jwt} = require('express-jwt');
+const jwksRsa = require('jwks-rsa');
 dotenv.config();
 const db = require('./queries')
 const cors = require('cors');
 
+const checkJwt = jwt({
+  // Dynamically provide a signing key based on the kid in the header and the signing keys provided by the JWKS endpoint
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://paddington.eu.auth0.com/.well-known/jwks.json`
+  }),
+  audience: 'https://paddington.eu.auth0.com/api/v2/', 
+  issuer: 'https://paddington.eu.auth0.com/',
+  algorithms: ['RS256']
+});
+
+app.use(checkJwt);
 app.use(bodyParser.json())
 app.use(
   bodyParser.urlencoded({
@@ -22,7 +38,7 @@ app.get('/', (request, response) => {
   response.json({ info: 'Hello World' })
 })
 
-app.get('/users/:id/todos', db.getTodos)
+app.get('/todos', db.getTodos)
 
 app.post('/todos', db.createTodos)
 
--- queries.js
+++ queries.js
@@ -1,32 +1,37 @@
+const jwt_decode = require('jwt-decode');
 const Pool = require('pg').Pool
 
-const pool = new Pool({  
-  host: process.env.HEROKU_DB_HOST,
-  database: process.env.HEROKU_DB_NAME,  
-  port: process.env.HEROKU_DB_PORT,
-  user: process.env.HEROKU_DB_USER,
-  password: process.env.HEROKU_DB_PASSWORD,  
+const connectionString = process.env.DATABASE_URL;
+
+const pool = new Pool({
+  connectionString,
   ssl: {
     rejectUnauthorized: false,
   },
 })
 
-const getTodos = async(request, response) => {
-  const id = parseInt(request.params.id);
-  pool.query('SELECT * FROM todos WHERE user_id = $1', [id], (error, results) => {
-    if (error) {
-      throw error
-    }
-    response.status(200).json(results.rows)
-  })
+const getTodos = async (request, response) => {
+  const { authorization } = request.headers
+  const payload = jwt_decode(authorization)
+  const { sub } = payload;
+  try {
+    const userResults = await pool.query('SELECT * FROM users WHERE auth_0_id = $1', [sub]);
+    const { id } = userResults.rows[0];
+    const todoResults = await pool.query('SELECT * FROM  todos WHERE user_id = $1', [id]);
+    console.log('TODOS QUERY', todoResults);
+    const { rows } = todoResults;
+    response.status(200).json(rows);
+  } catch (error) {
+    throw error;
+  }
 }
 
-const createTodos = async(request, response, next) => {
+const createTodos = async (request, response, next) => {
   try {
-    const { task, user_id } = request.body    
+    const { task, user_id } = request.body
     const todo = await pool.query('INSERT INTO todos (user_id, is_complete, task) VALUES  ($1, $2, $3)', [user_id, false, task])
     response.status(200).json(todo.rows[0]);
-  } catch(error) {
+  } catch (error) {
     next(error);
   }
 }
